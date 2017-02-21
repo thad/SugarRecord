ec7f3741b3aa892e062981ec2f777f271e0c78d3
Gleb <gleb.oleinik@gorillalogic.com>
2017-02-21 12:00:01 -0700

    implemented CoreDataMemoryStorage, to act as backup storage when disk storage fails

--- SugarRecord/Source/CoreData/Storages/CoreDataDefaultStorage.swift
+++ SugarRecord/Source/CoreData/Storages/CoreDataDefaultStorage.swift
@@ -1,6 +1,7 @@
 import Foundation
 import CoreData
 
+#if os(iOS) || os(tvOS) || os(watchOS)
 public class CoreDataDefaultStorage: Storage {
     
     // MARK: - Attributes
@@ -39,42 +40,6 @@ public class CoreDataDefaultStorage: Storage {
         return _context
     }
     
-    public func operation<T>(_ operation: @escaping (_ context: Context, _ save: @escaping () -> Void) throws -> T) throws -> T {
-        let context: NSManagedObjectContext = self.saveContext as! NSManagedObjectContext
-        var _error: Error!
-        
-        var returnedObject: T!
-        context.performAndWait {
-            do {
-                returnedObject = try operation(context, { () -> Void in
-                    do {
-                        try context.save()
-                    }
-                    catch {
-                        _error = error
-                    }
-                    self.rootSavingContext.performAndWait({
-                        if self.rootSavingContext.hasChanges {
-                            do {
-                                try self.rootSavingContext.save()
-                            }
-                            catch {
-                                _error = error
-                            }
-                        }
-                    })
-                })
-            } catch {
-                _error = error
-            }
-        }
-        if let error = _error {
-            throw error
-        }
-        
-        return returnedObject
-    }
-
     public func removeStore() throws {
         try FileManager.default.removeItem(at: store.path() as URL)
         _ = try? FileManager.default.removeItem(atPath: "\(store.path().absoluteString)-shm")
@@ -100,18 +65,8 @@ public class CoreDataDefaultStorage: Storage {
         versionController.check()
         #endif
     }
-    
-    
-    // MARK: - Public
-    
-#if os(iOS) || os(tvOS) || os(watchOS)
-    public func observable<T: NSManagedObject>(request: FetchRequest<T>) -> RequestObservable<T> where T:Equatable {
-        return CoreDataObservable(request: request, context: self.mainContext as! NSManagedObjectContext)
-    }
-#endif
-    
 }
-
+#endif
 
 // MARK: - Internal
 
---
+++ SugarRecord/Source/CoreData/Storages/CoreDataMemoryStorage.swift
@@ -0,0 +1,81 @@
+//
+//  CoreDataMemoryStorage.swift
+//  Carthage
+//
+//  Created by Gleb Oleinik on 2/16/17.
+//  Copyright © 2017 in.caramba.SugarRecord. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+#if os(iOS) || os(tvOS) || os(watchOS)
+public class MemoryStorage: Storage {
+    
+    // MARK: - Attributes
+    internal var objectModel: NSManagedObjectModel! = nil
+    internal var persistentStore: NSPersistentStore! = nil
+    internal var persistentStoreCoordinator: NSPersistentStoreCoordinator! = nil
+    internal var rootSavingContext: NSManagedObjectContext! = nil
+    
+    
+    // MARK: - Storage conformance
+    
+    public var description: String {
+        return "CoreDataMemoryStorage"
+    }
+    
+    public var type: StorageType = .coreData
+    public var mainContext: Context!
+    private var _saveContext: Context!
+    public var saveContext: Context! {
+        if let context = self._saveContext {
+            return context
+        }
+        let _context = memoryContext
+        _context.observe(inMainThread: true) { [weak self] (notification) -> Void in
+            (self?.mainContext as? NSManagedObjectContext)?.mergeChanges(fromContextDidSave: notification as Notification)
+        }
+        self._saveContext = _context
+        return _context
+    }
+    public var memoryContext: Context! {
+        let _context =  cdContext(withParent: .context(self.rootSavingContext), concurrencyType: .privateQueueConcurrencyType, inMemory: true)
+        return _context
+    }
+    
+    public func removeStore() throws {
+        // noop
+    }
+    
+    // MARK: - Init
+    public init(model: CoreDataObjectModel) throws {
+        self.objectModel = model.model()!
+        self.persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: objectModel)
+        self.persistentStore = try cdInitializeStore(storeCoordinator: persistentStoreCoordinator)
+        self.rootSavingContext = cdContext(withParent: .coordinator(self.persistentStoreCoordinator), concurrencyType: .privateQueueConcurrencyType, inMemory: true)
+        self.mainContext = cdContext(withParent: .context(self.rootSavingContext), concurrencyType: .mainQueueConcurrencyType, inMemory: true)
+    }
+}
+
+fileprivate func cdInitializeStore(storeCoordinator: NSPersistentStoreCoordinator) throws -> NSPersistentStore {
+    var persistentStore: NSPersistentStore?
+    var error: NSError?
+    storeCoordinator.performAndWait({ () -> Void in
+        do {
+            persistentStore = try storeCoordinator.addPersistentStore(ofType: NSInMemoryStoreType, configurationName: nil, at: nil, options: nil)
+        }
+        catch let _error as NSError {
+            error = _error
+        }
+    })
+    if let error = error {
+        throw error
+    }
+    guard let store = persistentStore else {
+        throw CoreDataError.persistenceStoreInitialization
+    }
+    
+    return store
+}
+#endif
